3380a5d5ea6169fd40059a2513510bfd70f6b48b
DM <dmiyamasu@gmail.com>
2023-03-29 13:32:00 -0400

    updated GA.js file url param

--- pages/_app.js
+++ pages/_app.js
@@ -64,16 +64,16 @@ const App = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
       </Head>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXX"></Script>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-CMP42YW7CX"></Script>
         <Script
-          id='google-analytics'
+          id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-CMP42YW7CX', {
+              gtag("js", new Date());
+              gtag("config", "G-CMP42YW7CX", {
                 page_path: window.location.pathname,
               });
             `,
